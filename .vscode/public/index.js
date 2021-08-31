f2d240573945181f18e90e67f37c34c28e898844
Aziz A.Nahli <aziz.abunahle@gmail.com>
2021-08-31 14:37:02 +0300

    starting build the front end side as template

---
+++ .vscode/public/index.js
@@ -0,0 +1,5 @@
+let typing = document.getElementById("playerName");
+typing.addEventListener('keyup', (event) => {
+    //here the connection to server start and we serach for competable matches
+     
+});
